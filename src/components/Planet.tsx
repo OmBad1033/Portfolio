00b4efdf2b20553d4b404e92c8c50b481b1ea820
OmBad1033 <om.badavanache05@gmail.com>
2025-06-29 19:48:30 +0530

    Add 3d model

---
+++ src/components/Planet.tsx
@@ -0,0 +1,77 @@
+import React, { useRef, type JSX } from "react";
+import { useGLTF } from "@react-three/drei";
+import { Group } from "three";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+type ModelProps = JSX.IntrinsicElements["group"]; // allow all <group> props
+
+export function Model(props: ModelProps) {
+  const shapeContainer = useRef(null);
+  const shereContainer = useRef(null);
+  const ringContainer = useRef(null);
+
+  const { nodes, materials } = useGLTF("/models/Planet.glb") as any;
+
+  useGSAP(()=>{
+    const tl = gsap.timeline();
+    tl.from(shapeContainer.current.position, {
+      y:5,
+      duration: 3,
+      ease: "power2.out",
+    })
+    tl.from(ringContainer.current.position, {
+      y:5,
+      duration: 2,
+      ease: "power2.out",
+    })
+    tl.from(ringContainer.current.rotation, {
+      x:0.8,
+      y:0,
+      z:0,
+      duration: 10,
+      ease: "power1.inOut",
+    })
+    tl.from(shereContainer.current.rotation, {
+      x:0,
+      y:Math.PI,
+      z:-Math.PI,
+      duration: 10,
+      ease: "power1.inOut",
+    },"-=25%");
+  })
+  return (
+    <group ref={shapeContainer} {...props} dispose={null}>
+      <group ref={shereContainer}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Sphere.geometry}
+          material={materials["Material.002"]}
+          rotation={[0, 0, 0.741]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Sphere2.geometry}
+          material={materials["Material.001"]}
+          position={[0.647, 1.03, -0.724]}
+          rotation={[0, 0, 0.741]}
+          scale={0.223}
+        />
+      </group>
+
+      <mesh
+        ref={ringContainer}
+        castShadow
+        receiveShadow
+        geometry={nodes.Ring.geometry}
+        material={materials["Material.001"]}
+        rotation={[-0.124, 0.123, -0.778]}
+        scale={2}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/models/Planet.glb");
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,10 +2,15 @@ import React, { useRef } from "react";
 import AnimatedTextLines from "../components/AnimatedTextLines";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Canvas } from "@react-three/fiber";
+import { Model } from "../components/Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
 type Props = {};
 
 function Hero({}: Props) {
+    const isMobile = useMediaQuery({maxWidth: 853});
   const contextRef = useRef(null);
   const headerRef = useRef(null);
   const aboutText = `I help growing brands and startups gain an
@@ -18,20 +23,24 @@ function Hero({}: Props) {
       y: "50vh",
       duration: 1,
       ease: "circ.out",
-    })
-    tl.from(headerRef.current, {
-      opacity: 0,
-      y: "200",
-      duration: 2,
-      ease: "circ.out",
-    },"<+1.2");
-  },[]);
+    });
+    tl.from(
+      headerRef.current,
+      {
+        opacity: 0,
+        y: "200",
+        duration: 2,
+        ease: "circ.out",
+      },
+      "<+1.2"
+    );
+  }, []);
 
   return (
     <section id="home" className="flex flex-col justify-end min-h-screen">
       <div ref={contextRef}>
         <div
-        className="border-t-1"
+          className="border-t-1"
           style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0 100%)" }}
         >
           <div
@@ -58,6 +67,53 @@ function Hero({}: Props) {
           </div>
         </div>
       </div>
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas
+          shadows
+          camera={{
+            position: [0, 0, -10],
+            fov: 17.5,
+            near: 1,
+            far: 20,
+          }}
+        >
+          <ambientLight intensity={0.5} />
+          <Float speed={0.5}>
+            <Model scale={isMobile ? 0.7 : 1} />
+          </Float> 
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[-5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
   );
 }
